eb34154f358d0ddb8c974323faba397078829ed0
unknown <webermarques9@gmail.com>
2018-11-25 19:05:20 -0200

    Mudanças

--- js/fakeDB.js
+++ js/fakeDB.js
@@ -6,8 +6,8 @@ var fakedba = {
             "formation": "1999",
             "genre": "Metal Core",
             "cover": "img/avenged-sevenfold.jpg",
-            "members":"M.Shadows, Synyster gates, Johnny Christ,Zacky Vengeance , Brooks Wackerman ",
-            "albums":"Sounding the 7th Trumpet, Walkin the Fallen , City of Evil, Avenged Seven Fold, Nightmare, Hail to the King,The Stage"
+            "members": "M.Shadows, Synyster gates, Johnny Christ,Zacky Vengeance , Brooks Wackerman ",
+            "albums": "Sounding the 7th Trumpet, Walkin the Fallen , City of Evil, Avenged Seven Fold, Nightmare, Hail to the King,The Stage"
 
         },
         {
@@ -16,8 +16,8 @@ var fakedba = {
             "formation": "1994",
             "genre": "Rock Alternativo",
             "cover": "img/muse.jpg",
-            "members":" Matthew Bellamy, Christopher Wolstenholme, Dominic Howard",
-            "albums":"Simulation Theory , Drones, Absolutin , Second Law, The Resistance"
+            "members": " Matthew Bellamy, Christopher Wolstenholme, Dominic Howard",
+            "albums": "Simulation Theory , Drones, Absolutin , Second Law, The Resistance"
         },
         {
             "id": 3,
@@ -25,8 +25,8 @@ var fakedba = {
             "formation": "1995",
             "genre": "Nu Metal",
             "cover": "img/slipknot.jpg",
-            "members":" Corey Taylor, Joey Jordison, Paul Gray, James Root",
-            "albums":" Iowa, All Hope Is Gone, Vol. 3: (The Subliminal Verses)"
+            "members": " Corey Taylor, Joey Jordison, Paul Gray, James Root",
+            "albums": " Iowa, All Hope Is Gone, Vol. 3: (The Subliminal Verses)"
         },
         {
             "id": 4,
@@ -34,17 +34,17 @@ var fakedba = {
             "formation": "1995",
             "genre": "Metal Alternativo",
             "cover": "img/chevelle.jpg",
-            "members":" Pete Loeffler, Sam Loeffler, Dean Bernardini",
-            "albums":"Sci-Fi crimes, Wonder Whats Next ,The North Corridor, !2 Bloody Spiders"
+            "members": " Pete Loeffler, Sam Loeffler, Dean Bernardini",
+            "albums": "Sci-Fi crimes, Wonder Whats Next ,The North Corridor, !2 Bloody Spiders"
         },
         {
             "id": 5,
             "nome": "Metallica",
             "formation": "1981",
-            "genre": "Heavy Metal", 
+            "genre": "Heavy Metal",
             "cover": "img/metallica.jpg",
-            "members":"Roberto Trujillo, James Hetfield, Lars Ulrich, Kirk Hammett,",
-            "albums":" Master of Puppets, Metallica, Ride the Lightning, Death Magnet"
+            "members": "Roberto Trujillo, James Hetfield, Lars Ulrich, Kirk Hammett,",
+            "albums": " Master of Puppets, Metallica, Ride the Lightning, Death Magnet"
         }
     ]
 };
@@ -67,33 +67,39 @@ function insertBand(banda) {
     let novaBanda = {
         "id": novoId,
         "nome": banda.nome,
-        "formation":banda.formation,
+        "formation": banda.formation,
         "genre": banda.genre,
         "cover": banda.cover,
         "members": banda.members,
         "albums": banda.albums
     };
 
-     // Insere o novo objeto no array
-     fakedb.data.push(novaBanda);
-     displayMessage("Sua banda foi inserida com sucesso");
- 
-     // Atualiza os dados no Local Storage
-     localStorage.setItem('fakedb', JSON.stringify(fakedb));
- }
+    // Insere o novo objeto no array
+    fakedb.data.push(novaBanda);
+    displayMessage("Sua banda foi inserida com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('fakedb', JSON.stringify(fakedb));
+}
+
+
+
 
 //ALTERAR UMA BANDA
- function updateBand(id, banda) {
+function updateBand(id, banda) {
     // Localiza o indice do objeto a ser alterado no array a partir do seu ID
     let index = fakedb.data.map(obj => obj.id).indexOf(id);
 
     // Altera os dados do objeto no array
-        fakedb.data[index].nome = banda.nome,
-        fakedb.data[index].formation = banda.formation,
-        fakedb.data[index].genre = banda.genre,
-        fakedb.data[index].cover = banda.cover,
-        fakedb.data[index].members = banda.members,
-        fakedb.data[index].albums = banda.albums,
+    fakedb.data[index].nome = banda.nome;
+    fakedb.data[index].formation = banda.formation;
+    fakedb.data[index].genre = banda.genre;
+    fakedb.data[index].members = banda.members;
+    fakedb.data[index].albums = banda.albums;
+    if(imagemA){
+        fakedb.data[index].cover = banda.cover;
+        imagemA = false;
+    }
 
     displayMessage("Banda alterada com sucesso");
 
@@ -116,3 +122,4 @@ function deleteBand(id) {
     // Limpa o formulario
     $("#form-banda")[0].reset();
 }
+
